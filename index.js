7e4620d258088b576baf0c9a5551182b1d9df07c
Michael Yogar <michaelyogar878@gmail.com>
2022-01-12 20:15:46 -0500

    feat: Added knex and morgan middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const { google } = require("googleapis");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -6,12 +7,37 @@ const axios = require("axios");
 const request = require("request");
 require("dotenv").config();
 
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  },
+});
+
 const app = express();
+app.use(morgan("dev"));
 const port = 1234;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await knex.select("name").from("company");
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      success: false,
+      message: "An error occurred, please try again later.",
+    });
+  }
+});
 
 app.get("/getURL", (req, res) => {
   const oauth2Client = new google.auth.OAuth2(
@@ -51,7 +77,7 @@ app.get("/test", async (req, res) => {
     process.env.REDIRECT
   );
   const tokens = await oauth2Client.getToken(req.query.code);
-  res.send("success");
+  https: res.send("success");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
